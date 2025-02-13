939a9b0f7e1c215d6a318ce276593543f1d4eed0
CYRYGBG <20202232015@m.scnu.edu.cn>
2025-02-14 00:32:59 +0800

    running

--- Code/LLM03/night_run.sh
+++ Code/LLM03/night_run.sh
@@ -1,4 +1,4 @@
-bash .\train_sh\rslora16.sh 
-bash .\train_sh\lora64.sh 
-bash .\train_sh\loraplus64.sh 
-bash .\train_sh\rslora64.sh 
+bash ./train_sh/rslora16.sh 
+bash ./train_sh/lora64.sh 
+bash ./train_sh/loraplus64.sh 
+bash ./train_sh/rslora64.sh 
