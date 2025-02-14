9a18c717212c251c5e335b5c49e9ad5415d248be
fbanhua <20202232015@m.scnu.edu.cn>
2025-02-15 01:10:40 +0800

    all_eval.sh update

---
+++ Code/LLM03/best_merge.sh
@@ -0,0 +1,10 @@
+llamafactory-cli export ./best_merge/dora8.yaml
+llamafactory-cli export ./best_merge/dora16.yaml
+llamafactory-cli export ./best_merge/dora512.yaml
+llamafactory-cli export ./best_merge/lora512.yaml
+llamafactory-cli export ./best_merge/loraplus512.yaml
+llamafactory-cli export ./best_merge/rslora8.yaml
+llamafactory-cli export ./best_merge/rslora16.yaml
+llamafactory-cli export ./best_merge/rslora512.yaml
+llamafactory-cli export ./best_merge/pissa8.yaml
+
---
+++ Code/LLM03/final_merge.sh
@@ -0,0 +1,19 @@
+llamafactory-cli export ./final_merge/lora8.yaml
+llamafactory-cli export ./final_merge/lora16.yaml
+llamafactory-cli export ./final_merge/lora512.yaml
+
+llamafactory-cli export ./final_merge/loraplus8.yaml
+llamafactory-cli export ./final_merge/loraplus16.yaml
+llamafactory-cli export ./final_merge/loraplus512.yaml
+
+llamafactory-cli export ./final_merge/rslora8.yaml
+llamafactory-cli export ./final_merge/rslora16.yaml
+llamafactory-cli export ./final_merge/rslora512.yaml
+
+llamafactory-cli export ./final_merge/dora8.yaml
+llamafactory-cli export ./final_merge/dora16.yaml
+llamafactory-cli export ./final_merge/dora512.yaml
+
+llamafactory-cli export ./final_merge/pissa8.yaml
+llamafactory-cli export ./final_merge/pissa16.yaml
+llamafactory-cli export ./final_merge/pissa512.yaml
